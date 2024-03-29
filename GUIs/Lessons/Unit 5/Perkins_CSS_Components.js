c44acc46a0eb7382086c63c1edc03f09e63c3854
Sam Perkins workt <perkinssa8@gmail.com>
2024-03-29 16:59:35 -0600

    Fear and Fridays @5:00

--- GUIs/Lessons/Unit 5/Perkins_CSS_Components.js
+++ GUIs/Lessons/Unit 5/Perkins_CSS_Components.js
@@ -1,16 +1,3 @@
-//canvas practice
-const canvas = document.getElementById('canvas');
-
-const ctx = canvas.getContext('2d');
-
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(50,50, 100, 100);
-
-ctx.fillStyle = "#00FF00";
-ctx.fillRect(200, 50, 100, 100);
-
-
-
 const fabButton = document.querySelector('.button');
 
 fabButton.addEventListener('mousedown', (e) => {
@@ -26,32 +13,12 @@ fabButton.addEventListener('mouseleave', (e) => {
     e.target.dataset.pressed = 'false';
 })
 
-const container = document.querySelector('.container');
-const loader = document.querySelector('.loader');
-
-container.addEventListener('mousedown', (e) => {
-    const animation = loader.animate(
-        [{
-            transform: `translate(${e.clientX - 64}px, ${e.clientY - 64}px)`
-        }],
-        {
-            duration: 300,
-            fill: 'forwards',
-            easing: 'ease-in-out'
-        })
-        animation.addEventLister('finish', () => {
-            // loader.style.display = 'block';
-            console.log('animation finished');
-        });
-})
-
 
 // drawer 
 let isNavOpen = false;
-const drawer = document.getElementById("drawer");
+const drawer = document.querySelector("#drawerOptions");
 
-document.getElementById("menu").addEventListener("click", () => {
-    console.log("menu clicked");
+document.getElementById("drawerMenu").addEventListener("click", () => {
+    isNavOpen = !isNavOpen;
+    drawer.dataset.open = `${isNavOpen}`;
 });
-
-
