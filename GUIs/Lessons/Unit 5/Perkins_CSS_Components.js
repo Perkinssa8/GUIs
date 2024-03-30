73b9d2c189bc889bd5d9631cd388fedd33679e75
Perkinssa8 <perkinssa8@gmail.com>
2024-03-30 00:56:16 -0600

    Getting to the image carousel @1

--- GUIs/Lessons/Unit 5/Perkins_CSS_Components.js
+++ GUIs/Lessons/Unit 5/Perkins_CSS_Components.js
@@ -22,3 +22,23 @@ document.getElementById("drawerMenu").addEventListener("click", () => {
     isNavOpen = !isNavOpen;
     drawer.dataset.open = `${isNavOpen}`;
 });
+
+
+// Image Carousel
+const carousel = document.querySelectorAll('.carousel-item');
+console.log(carousel);
+let carouselCopy = [...carousel];
+
+
+[...carousel].forEach((image) => {
+    //1. nothing here
+    setTimeout(() => {
+        // 1. for each image, create an element of type img
+        document.querySelector('#carousel').remove();
+        // 1.b for each replace the src of the image with the next image in the array
+        document.querySelector('#carousel').appendChild(image);
+        // 2. add a class to the image focused in the carousel
+        // 3. remove the class from the image that is no longer in focus
+        // 4. change the order of the original array: move the first image to the end of the array
+        }, index * 1000); // Change the delay time (in milliseconds) as per your requirement
+})
