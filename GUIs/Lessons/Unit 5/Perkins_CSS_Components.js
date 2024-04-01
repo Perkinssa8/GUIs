c8b4d3202456689bf863d921ef325b87f38dc4c3
Perkinssa8 <perkinssa8@gmail.com>
2024-04-01 00:38:25 -0600

    Finally done, why do I do this to myself? @12:40am

--- GUIs/Lessons/Unit 5/Perkins_CSS_Components.js
+++ GUIs/Lessons/Unit 5/Perkins_CSS_Components.js
@@ -1,3 +1,13 @@
+let NavOpen = false;
+const drawer2 = document.querySelector("#mainDrawer");
+
+document.getElementById("hamburger").addEventListener("click", () => {
+    window.scrollTo(0, 0);
+    NavOpen = !NavOpen;
+    drawer2.dataset.open = `${NavOpen}`;
+});
+
+//fabButton
 const fabButton = document.querySelector('.button');
 
 fabButton.addEventListener('mousedown', (e) => {
@@ -25,20 +35,52 @@ document.getElementById("drawerMenu").addEventListener("click", () => {
 
 
 // Image Carousel
-const carousel = document.querySelectorAll('.carousel-item');
-console.log(carousel);
-let carouselCopy = [...carousel];
+const carouselHTML = document.querySelectorAll('.carousel-item');
+
+class ImageCarousel {
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.parent = document.querySelector('#carousel');
+        this.carouselArray = Array.from(this.carousel);
+    }
+
+    next() {
+        this.carouselArray[2].dataset.on = 'false';
+        const firstImage = this.carouselArray.shift();
+        this.carouselArray.push(firstImage);
+        this.carouselArray[2].dataset.on = 'true';
+    }
+
+    getImages() {
+        return this.carousel;
+    }
+
+    render() {
+        this.parent.innerHTML = '';
+        this.carouselArray.forEach((image) => {
+            this.parent.appendChild(image);
+        });
+    }
+}
 
 
-[...carousel].forEach((image) => {
-    //1. nothing here
-    setTimeout(() => {
-        // 1. for each image, create an element of type img
-        document.querySelector('#carousel').remove();
-        // 1.b for each replace the src of the image with the next image in the array
-        document.querySelector('#carousel').appendChild(image);
-        // 2. add a class to the image focused in the carousel
-        // 3. remove the class from the image that is no longer in focus
-        // 4. change the order of the original array: move the first image to the end of the array
-        }, index * 1000); // Change the delay time (in milliseconds) as per your requirement
-})
+const carousel = document.querySelector('#carousel');
+
+let play = new ImageCarousel(carouselHTML);
+
+carousel.addEventListener('animationiteration', () => {
+    play.next();
+    play.render();
+});
+
+const clickLoader = document.querySelector('.loader');
+
+clickLoader.addEventListener('click', () => {
+    clickLoader.dataset.pressed = 'true';
+    clickLoader.dataset.open = 'true';
+});
+
+clickLoader.addEventListener('animationiteration', () => {
+    clickLoader.dataset.pressed = 'false';
+});
+
