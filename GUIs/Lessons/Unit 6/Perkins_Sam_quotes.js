3ef60ff14da184709e2e2a38c533e2aa1eca378b
Perkinssa8 <perkinssa8@gmail.com>
2024-04-07 23:42:50 -0600

    3 body problem

---
+++ GUIs/Lessons/Unit 6/Perkins_Sam_quotes.js
@@ -0,0 +1,13 @@
+async function getQuote () {
+    const res = await fetch("https://api.quotable.io/random")
+    const body = await result.json();
+    return body;
+};
+
+document.body.addEventListener("click", async () => {
+    const quote = await getQuote();
+    document.getElementById("quote-content").innerText = quote.content;
+    document.getElementById("quote-author").innerText = `- ${quote.author}`;
+    console.log(quote);
+
+})
