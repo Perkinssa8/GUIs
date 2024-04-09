d03556733b5ed74c26f594fa8cd744405abbe314
Sam Perkins <perkinssa8@gmail.com>
2024-04-09 11:30:09 -0600

    test day

--- GUIs/Lessons/Unit 6/Perkins_Sam_quotes.js
+++ GUIs/Lessons/Unit 6/Perkins_Sam_quotes.js
@@ -1,5 +1,5 @@
 async function getQuote () {
-    const res = await fetch("https://api.quotable.io/random")
+    const result = await fetch("https://api.quotable.io/random")
     const body = await result.json();
     return body;
 };
@@ -11,3 +11,23 @@ document.body.addEventListener("click", async () => {
     console.log(quote);
 
 })
+
+let openBar = false;
+document.getElementById("search").addEventListener("click", () => {
+
+    openBar = !openBar;
+    if (openBar) {
+        document.getElementById("search-input").style.display = "block";
+        document.getElementById("navBarTitle").style.display = "none";
+    } else {
+        document.getElementById("search-input").style.display = "none";
+        document.getElementById("navBarTitle").style.display = "block"
+    }
+});
+async function searchQuote() {
+    const search = await document.getElementById("search-input").value;
+}
+
+
+const wrap = (arg1)=> (arg2) => (arg3) => `${arg1} - ${arg2} - ${arg3}`;
+console.log(wrap("Hello, world"));
